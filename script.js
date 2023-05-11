dffe43aff60ab24fa0750ca04cf2e5727e3fbf97
salma kandeel <68400035+salmakandeel@users.noreply.github.com>
2023-05-11 14:25:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ function increase(){
     counter++
     console.log(counter);
     root.innerHTML = counter;
+    root.style.color='green'
 }
 function decrease(){
     counter--
     console.log(counter);
    root.innerHTML=counter
+   root.style.color='red'
 }
 function reset(){
     counter=0
     console.log(counter);
     root.innerHTML=counter
+    root.style. color= rgb(133, 133, 215);
 }
